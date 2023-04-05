a55c27c477bd37729c73d5b1d28ac3bbce070ef2
jinsung <you@example.com>
2023-04-05 21:04:19 +0700

    google redirecturl fixed

--- auth/google_login.py
+++ auth/google_login.py
@@ -18,7 +18,7 @@ environment = os.environ.get('ENVIRONMENT')
 redirect_url = None
 
 if environment == 'production':
-    redirect_url = "https://ieunghieut-frontend.pages.dev/auth/google/callback"
+    redirect_url = "https://ieunghieut-frontend.pages.dev/login/google"
 else:
     redirect_url = f"http://127.0.0.1:8000/auth/google/callback"
 
